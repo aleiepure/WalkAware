c28500da36751811dd38aa676dba2316f283523a
Alessandro Iepure <39530238+aleiepure@users.noreply.github.com>
2024-04-27 09:56:17 +0200

    Initial commit: template projects

---
+++ README.md
@@ -0,0 +1,13 @@
+# WalkAware Trento
+
+Progetto per il corso di Ingegneria del software, prof. Sandro Fiore - Università degli studi di Trento.
+
+## Organizzazione della repo
+
+```shell
+├── backend
+├── frontend
+│   ├── mobile
+│   └── webapp
+└── readme.md
+```
---
+++ backend/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Hello World',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./index.js', './src/routes/*.js'], // files containing annotations as above
+};
+
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+// Serving static files
+app.use(express.static('public'));
+
+// Handling GET requests
+const swaggerDocument = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocument);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    explorer: true
+}));
+
+/**
+* @openapi
+* /:
+*   get:
+*       description: Welcome to swagger-jsdoc!
+*       responses:
+*           200:
+*               description: Returns a mysterious string.
+*/
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// Run the application on port 3000
+app.listen(3000, function () {
+    console.log('Server running on port ', 3000);
+});
