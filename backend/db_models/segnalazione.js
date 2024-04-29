49caf3093acd7680093402f8a5b8685be4fc501b
carlottacazzolli <carlottacazzolli02@gmail.com>
2024-04-29 12:21:45 +0200

    seconda versione del database: aggiunta moduli segnalaizone, azienda, attrazioene premio

---
+++ backend/db_models/segnalazione.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+module.exports("segnalazione", new Schema({
+    id: Schema.Types.ObjectId, 
+    luogo: Number, 
+    foto: String,  /*TODO: verify how to actually store an image*/
+    tipo: ["strada", "illuminazione", "segnaletica", "sicurezza", "barriereArchitettoniche"],
+    urgenza: ["bassa", "medio-bassa", "medio-alta", "alta"],
+    status: ["aperta", "presa_in_carico", "conclusa"]
+}))
