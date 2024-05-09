5b3082ceb781950fccd877e1dde7c4d41f168cba
Alessandro Iepure <39530238+aleiepure@users.noreply.github.com>
2024-05-09 14:07:49 +0200

    Mobile: Sistemate richieste al backend che richiedono l'autenticazione Frontend: Aggiunto nome della pagina nel titolo

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,15 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-
-
 const port = process.env.PORT || 8080;
-
-
 
 app.locals.db = mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
-
-        console.log("Connected to Database");
-
+        console.log("Connected to database");
         app.listen(port, () => {
             console.log(`Server listening on port ${port}`);
         });
-
+    })
+    .catch((err) => {
+        console.error("Cannot connect to the database");
     });
